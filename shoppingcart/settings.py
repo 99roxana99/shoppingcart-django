b52b1cf5f3eae35458b1f6377fd488129d337a10
01elenaroxana02@gmail.com <01elenaroxana02@gmail.com>
2021-11-11 21:12:18 +0200

    django authentication

--- shoppingcart/settings.py
+++ shoppingcart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+#from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -135,3 +136,16 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '01elenaroxana02@gmail.com'
+EMAIL_HOST_PASSWORD = '99elena.roxana99'
+EMAIL_USE_TLS = True
